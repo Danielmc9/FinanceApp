db04fdb2fc383387fb6bf794973602cc4fea0aee
Danielmc9 <danmartinezco@gmail.com>
2026-04-30 18:31:28 -0500

    feat(ui): Color.kt and Theme.kt created whith color and theme setting

--- app/src/main/java/com/example/financeapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/financeapp/ui/theme/Color.kt
@@ -2,10 +2,42 @@ package com.example.financeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleGrey80 = Color(0xFFDBBCFF)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
 val Pink40 = Color(0xFF7D5260)
+
+
+
+val PrimaryGreen = Color(0xFF00C853)
+val PrimaryGreenDark = Color(0xFF00A043)
+val SecondaryBlue = Color(0xFF2196F3)
+val AccentOrange = Color(0xFFFF9800)
+
+//Colores de fondo
+
+val BackgroundLight = Color(0xFFF6F8FA)
+val SurfaceLight = Color(0xFFFFFFFF)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+
+//colores estado
+
+val SuccessGreen = Color(0xFF4CAF50)
+val ErrorRed = Color(0xFFF44336)
+val WarningYellow = Color(0xFFFFC107)
+val InfoBlue = Color(0xFF2196F3)
+
+//colores de categorias
+
+val Food = Color(0xFFFF6B6B)
+val Transport = Color(0xFF4ECDC4)
+val Entertainment = Color(0xFFFFBE0B)
+val Bills = Color(0xFF8C5CF6)
+val Shopping = Color(0xFFEC4899)
+val Health = Color(0xFF10B981)
+val Education = Color(0xFF3B82F6)
+val Travel = Color(0xFFF59E0B)
+val Other = Color(0xFF6B7280)
